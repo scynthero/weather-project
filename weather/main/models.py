6ac4369f933cb7c10487f8f2fe9ef4b26b3b57e1
Filip Masiukiewicz <scynthero@outlook.com>
2016-06-10 16:17:29 +0200

    model, urls, app

--- weather/main/models.py
+++ weather/main/models.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
+
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=40)
+
+#    class Meta:
+#        verbose_name = "Miasto"
+#        verbose_name_plural = "Miasta"
+
+    def __str__(self):
+        return self.name
--- weather/main/views.py
+++ weather/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import City
+
 
 # Create your views here.
+
+def index(request):
+    cities = City.objects.all().order_by('name')
+    return render(request, 'main/index.html', {'cities':cities})
